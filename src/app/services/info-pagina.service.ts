cb251ea0b031fed45279dcbc56bd1f7e8ead485e
Marco Andres <marco19398@gmail.com>
2020-01-22 10:58:20 -0600

    Fin seccion 7

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,17 +7,40 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 })
 export class InfoPaginaService {
 
-  info: InfoPagina ={};
+  info: InfoPagina = {};
   cargada = false;
 
-  constructor(private http: HttpClient) { 
-    //console.log('Servicio de info pagina listo');
+  equipo: any[] = [];
 
+  constructor( private http: HttpClient ) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
+    // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-    .subscribe(resp => {
+    .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
   }
+
+
+  private cargarEquipo() {
+
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-25cf9.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      // console.log(resp);
+    });
+
+
+    // this.equipo = resp
+  }
+
 }
